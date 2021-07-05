fdd7a7a52c5f529fb333a2686ff627c9cbe085ab
scinorandex <scinorandex1@gmail.com>
2021-07-05 19:02:47 +0800

    add help message

--- src/cli.ts
+++ src/cli.ts
@@ -7,12 +7,41 @@ import { dashboardSizeError, sizes } from "./routers/dashboardRouter";
 import { modeNotFoundError, modes } from "./routers/mainRouter";
 import { Mode } from "./types/schema";
 import { DashboardSize, MainFunction, ObjectIface } from "./types/types";
+import { lines, welcomeMessage } from "./utils/lines";
 import {
     countryData,
     countryHistorical,
     globalData,
     globalHistorical,
 } from "./utils/main";
+
+const runHelp = () => {
+    console.log(`${welcomeMessage}
+Usage: covid [COUNTRY] [OPTIONS...]
+Country:    Can be a country name or ISO 3166-1 alpha-2 or alpha-3 country code
+            Ex: ph = Philippines, kr = South Korea
+            Leave empty to show global data
+
+Options:
+    --help    Show this help message
+    -q, --quiet     Only show necessary information
+    -p, --plain     Enable plain mode
+    -d, --dashboard Show a dashboard view
+    -h, --history   Show a chart of country's or world's cases
+    --size      Use with --dashboard to control the size of the output.
+    --mode      Use with --history to show a chart of cases, deaths, or recovered.
+    
+    Sizes:  [${sizes.join(" | ")}]
+    Modes:  [${modes.join(" | ")}]
+		
+Useful Links:
+	${lines.docsLink}
+	${lines.WNrepoLink}
+        ${lines.WNDonateLink}
+`);
+
+    process.exit(0);
+};
 
 // prettier-ignore
 (async () => {
@@ -25,6 +54,7 @@ import {
 
     const country = argv._.shift();
     let params = {
+		help: argv.help,
 		quiet: argv.q || argv.quiet,
         plain: argv.p || argv.plain,
         dashboard: argv.d || argv.dashboard,
@@ -36,6 +66,7 @@ import {
         if (params[key] === undefined) params[key] = false;
     });
 
+	if(params.help === true) return runHelp();
 	let callbackFunction = params.dashboard ? dashboardHandler(true, size) :
 							params.plain ? plainHandler(params.quiet):
 										regularHandler(params.quiet);
